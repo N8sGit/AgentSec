bcaefcfb3ebe35610d9dc8c9241aca22f01965e2
Nathan Anecone <nanecone@gmail.com>
2024-12-08 14:56:24 -0500

    Add ids to pydantic models

--- messages/messages.py
+++ messages/messages.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import Optional
+import uuid
 
 class AuthUserMessage(BaseModel):
     message: str
@@ -9,6 +10,7 @@ class AuthUserMessage(BaseModel):
 class InstructionMessage(BaseModel):
     message: str
     timestamp: int
+    id: str = str(uuid.uuid4()) 
     sender: str
     token: str
     signature: str = Field(..., description="Digital signature for message authentication")
@@ -17,4 +19,5 @@ class DataMessage(BaseModel):
     message: str
     timestamp: int
     sender: str
+    id: str = str(uuid.uuid4()) 
     clearance_level: Optional[int] = Field(None, description="Optional to support unclassified data")
